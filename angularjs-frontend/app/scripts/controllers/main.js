347c0d897c6b415ff2285ac6a1ad4a638f447ed7
Christopher Bartling <chris.bartling@gmail.com>
2014-01-19 15:11:57 -0600

    Created the AngularJS front-end project.

---
+++ angularjs-frontend/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularjsFrontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
